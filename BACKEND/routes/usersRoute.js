0b799758a201174e2fcada3f807ce5f6804495ee
yassineelmiri <yassireelhima@gmail.com>
2024-10-06 16:50:34 +0100

    Ajout des routes pour les utilisateurs

--- BACKEND/routes/usersRoute.js
+++ BACKEND/routes/usersRoute.js
@@ -1,7 +1,32 @@
 const router = require("express").Router();
-const { getAllUsersCtrl } = require("../controllers/userController");
-const { VerifyToken } = require("../middlewares/verifyToken");
+const {
+  getAllUsersCtrl,
+  getUsersProfileCtrl,
+  updateUserProfileCtrl,
+  getUsersCountCtrl,
+  profilePhotoUploadCtrl,
+} = require("../controllers/userController");
+const {
+  verifyTokenAndAdmin,
+  verifyTokenAndOnlyUser,
+  VerifyToken,
+} = require("../middlewares/verifyToken");
+const validateObjectId = require("../middlewares/validateObjectId");
+const photoUpload = require("../middlewares/photoUpload");
 
-router.route("/profile").get( VerifyToken ,getAllUsersCtrl);
+
+
+router.route("/profile").get(verifyTokenAndAdmin, getAllUsersCtrl);
+
+router.route("/profile/:id")
+  .get(validateObjectId, getUsersProfileCtrl)
+  .put(validateObjectId, verifyTokenAndOnlyUser, updateUserProfileCtrl);
+
+
+
+router.route("/profile/profile-photo-upload").post(VerifyToken,photoUpload.single("image") ,profilePhotoUploadCtrl);
+
+
+router.route("/count").get(verifyTokenAndAdmin, getUsersCountCtrl);
 
 module.exports = router;
