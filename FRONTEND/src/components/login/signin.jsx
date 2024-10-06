ba969dfd1a27308ebb1e9bd2646676085a996174
yassineelmiri <yassireelhima@gmail.com>
2024-10-06 16:50:20 +0100

    Ajout de la page de connexion

--- FRONTEND/src/components/login/signin.jsx
+++ FRONTEND/src/components/login/signin.jsx
@@ -1,37 +1,99 @@
-import React from "react";
-import '../../assets/css/main.css';
-
-// Importez les images que vous souhaitez afficher
-import logo from '../../assets/img/logo.svg'; // Assurez-vous que le chemin est correct
-import bgImage from '../../assets/img/bg.jpg'; // Assurez-vous que le chemin est correct
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../../assets/css/main.css";
+import '../../assets/css/bootstrap-grid.min.css';
+import '../../assets/css/bootstrap-reboot.min.css';
+import '../../assets/css/magnific-popup.css';
+import '../../assets/css/plyr.css';
+import '../../assets/css/select2.min.css';
+import '../../assets/css/slider-radio.css';
+import '../../assets/css/owl.carousel.min.css';
+import { toast } from "react-toastify"; // Pour afficher des notifications
+import { useDispatch, useSelector } from "react-redux";
+import logo from "../../assets/img/series/logo.png"; // Logo de l'application
+import bgImage from "../../assets/img/bg.jpg"; // Image de fond
+import { loginUser } from "../../redux/apiCalls/authApiCall"; // Appel API pour la connexion
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  
+  // Récupérer l'utilisateur depuis le store
+  const { user } = useSelector(state => state.auth);
+
+  // Gestion de la soumission du formulaire
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    // Validation simple
+    if (email.trim() === "") {
+      return toast.error("Email is required");
+    }
+    if (password.trim() === "") {
+      return toast.error("Password is required");
+    }
+
+    // Appel à la fonction de connexion
+    dispatch(loginUser({ email, password }));
+  };
+
+  // Afficher un pop-up si l'utilisateur est connecté
+  useEffect(() => {
+    if (user) {
+      toast.success("Successfully logged in!"); // Afficher le pop-up de succès
+    }
+  }, [user]); // Dépend de l'utilisateur
+
   return (
-    <div className="sign section--full-bg" style={{ backgroundImage: `url(${bgImage})` }}> {/* Utilisation de l'image de fond */}
+    <div
+      className="sign section--full-bg"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       <div className="container">
         <div className="row">
           <div className="col-12">
             <div className="sign__content">
-              <form className="sign__form">
-                <a href="/" className="sign__logo">
-                  <img src={logo} alt="Logo" /> {/* Affichage du logo */}
-                </a>
+              <form onSubmit={formSubmitHandler} className="sign__form">
+                <Link to="/" className="sign__logo">
+                  <img src={logo} alt="Logo" />
+                </Link>
                 <div className="sign__group">
-                  <input type="email" className="sign__input" placeholder="Email" />
+                  <input
+                    type="email"
+                    className="sign__input"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
                 <div className="sign__group">
-                  <input type="password" className="sign__input" placeholder="Password" />
+                  <input
+                    type="password"
+                    className="sign__input"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </div>
                 <div className="sign__group sign__group--checkbox">
-                  <input id="remember" name="remember" type="checkbox" defaultChecked />
-                  <label htmlFor="remember">Remember Me</label>
+                  <input
+                    id="remember"
+                    name="remember"
+                    type="checkbox"
+                    defaultChecked
+                  />
                 </div>
-
-                <button className="sign__btn" type="button">Sign In</button>
-
+                <button className="sign__btn" type="submit">
+                  Sign In
+                </button>
                 <span className="sign__delimiter">or</span>
-                <span className="sign__text">Don't have an account? <a href="/signup">Sign up!</a></span>
-                <span className="sign__text"><a href="/forgot">Forgot password?</a></span>
+                <span className="sign__text">
+                  Don't have an account? <Link to="/signup">Sign up!</Link>
+                </span>
+                <span className="sign__text">
+                  <Link to="/forgot">Forgot password?</Link>
+                </span>
               </form>
             </div>
           </div>
