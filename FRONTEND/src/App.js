4c5e0bc957ec571d18f770b19f8705e0225658c4
yassineelmiri <yassireelhima@gmail.com>
2024-10-06 16:50:32 +0100

    Configuration de l'application principale

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,15 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/index";
-import SignIn from "./components/login/signin";
-import SignUp from "./components/login/signup";
+import SignIn from "./Pages/forms/Login.jsx";
+import SignUp from "./Pages/forms/Register.jsx";
+import PostsPage from "./Pages/posts/PostsPage.jsx";
+import CreatePost from "./Pages/create-post/CreatePost.jsx";
+import Error from "./Pages/404/error.jsx";
+import AdminDashbord from './Pages/Admin/AdminDashbord';
+import PostDetail from "./Pages/posts/PostDetail.jsx";
+import ForgotPassword from "./components/login/forgotpassword.jsx";
+import ResetPassword from "./components/login/resetpassword.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts/create-post" element={<CreatePost />} />
+        <Route path="/posts/details/:id" element={<PostDetail />} />
+
+        <Route path="/admin" element={<AdminDashbord />} />
+        
+        <Route path="*" element={<Error />} />
 
       </Routes>
     </BrowserRouter>
