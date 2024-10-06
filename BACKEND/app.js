4fac56e779751dbe35276eecc3d6f59ecd1445c6
yassineelmiri <yassireelhima@gmail.com>
2024-10-06 16:50:38 +0100

    Configuration principale de l'application

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const ConnectToDb = require("./config/connectToDb");
+const cors = require("cors");
 require("dotenv").config();
 ConnectToDb();
 
 const app = express();
 
-//Middlewares
+// Middlewares
 app.use(express.json());
 
-//Route
+//cors policy
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
+
+
+// Routes
 app.use("/api/auth", require("./routes/authRoute"));
 app.use("/api/users", require("./routes/usersRoute"));
+app.use("/api/posts", require("./routes/postsRoute"));
 
 // Running The Server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>
-  console.log(`Server is runing in ${process.env.NODE_ENV} mode on port `)
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
 );
