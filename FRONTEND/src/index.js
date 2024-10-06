17b76e3de3d0d64b823bb5411e28d1f597c38f45
yassineelmiri <yassireelhima@gmail.com>
2024-10-06 16:50:33 +0100

    Point d'entrée de l'application

--- FRONTEND/src/index.js
+++ FRONTEND/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "react-toastify/ReactToastify.css";
+import "./index.css";
+import App from "./App";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
