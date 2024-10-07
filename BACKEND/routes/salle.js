b394b899bf5c3140916791be367f64370651351d
yassineelmiri <yassireelhima@gmail.com>
2024-10-07 23:37:54 +0100

    Ajout de nouvelles routes pour la gestion des salles, avec des filtres par capacité et disponibilité.

---
+++ BACKEND/routes/salle.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const SalleController = require("../controllers/SalleController");
+const authMiddleware = require("../middleware/auth");
+
+router.post("/", SalleController.addSalle);
+router.get("/", SalleController.getSalles);
+router.put("/:id", SalleController.updateSalle);
+router.delete("/:id", SalleController.deleteSalle);
+
+module.exports = router;
